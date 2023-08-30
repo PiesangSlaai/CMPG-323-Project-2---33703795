cff1743413083918066be9a441187a42bc9c45b8
PiesangSlaai <ziankara0@gmail.com>
2023-08-30 22:20:35 +0200

    Completed OrderController and all related methods

---
+++ CMPG 323 Project 2 - 33703795/Controllers/OrderController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMPG_323_Project_2___33703795.Models;
+
+namespace CMPG_323_Project_2___33703795.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly cmpg323_Project2_APIContext _context;
+
+        public OrderController(cmpg323_Project2_APIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Order/customer/5
+        [HttpGet("customer/{customerId}")]
+        public ActionResult<IEnumerable<Order>> GetOrdersForCustomer(int customerId)
+        {
+            var orders = _context.Orders
+                .Where(o => o.CustomerId == customerId)
+                .ToList();
+
+            if (orders.Count == 0)
+            {
+                return NotFound("No orders found for the specified customer.");
+            }
+
+            return Ok(orders);
+        }
+
+        // GET: api/Order
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+          if (_context.Orders == null)
+          {
+              return NotFound();
+          }
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(short id)
+        {
+          if (_context.Orders == null)
+          {
+              return NotFound();
+          }
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Order/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(short id, Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return BadRequest();
+            }
+            if (!OrderExists(id))
+            {
+                return NotFound();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Order
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+          if (_context.Orders == null)
+          {
+              return Problem("Entity set 'cmpg323_Project2_APIContext.Orders'  is null.");
+          }
+            _context.Orders.Add(order);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderExists(order.OrderId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOrder", new { id = order.OrderId }, order);
+        }
+
+        // DELETE: api/Order/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(short id)
+        {
+            if (_context.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            if (!OrderExists(id))
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(short id)
+        {
+            return (_context.Orders?.Any(e => e.OrderId == id)).GetValueOrDefault();
+        }
+    }
+}
